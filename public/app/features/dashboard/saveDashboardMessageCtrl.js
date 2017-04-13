60cf995e2fdcaf50607d25b6baf03c4ffbd6282a
Carlos Rosquillas <crosquillas@gmail.com>
2017-04-12 18:30:24 -0700

    Add error handling to save message controller
    - Use all error handling conditions from original `saveDashboard` in new message-based save
    - Update audit log on dashboard save
    - Deduplicate spys and stubs

--- public/app/features/dashboard/saveDashboardMessageCtrl.js
+++ public/app/features/dashboard/saveDashboardMessageCtrl.js
@@ -6,7 +6,7 @@ function (angular) {
 
   var module = angular.module('grafana.controllers');
 
-  module.controller('SaveDashboardMessageCtrl', function($scope, backendSrv, $location) {
+  module.controller('SaveDashboardMessageCtrl', function($scope, $location, backendSrv, dashboardSrv) {
 
     $scope.init = function() {
       $scope.clone.message = '';
@@ -27,6 +27,21 @@ function (angular) {
 
     $scope.saveVersion = function() {
       saveDashboard({overwrite: false}).then(null, function(err) {
+        if (err.data && err.data.status === "version-mismatch") {
+          err.isHandled = true;
+
+          $scope.appEvent('confirm-modal', {
+            title: 'Conflict',
+            text: 'Someone else has updated this dashboard.',
+            text2: 'Would you still like to save this dashboard?',
+            yesText: "Save & Overwrite",
+            icon: 'fa-warning',
+            onConfirm: function() {
+              saveDashboard({overwrite: true});
+            }
+          });
+        }
+
         if (err.data && err.data.status === "name-exists") {
           err.isHandled = true;
 
@@ -35,7 +50,26 @@ function (angular) {
             text: 'Dashboard with the same name exists.',
             text2: 'Would you still like to save this dashboard?',
             yesText: "Save & Overwrite",
-            icon: "fa-warning",
+            icon: 'fa-warning',
+            onConfirm: function() {
+              saveDashboard({overwrite: true});
+            }
+          });
+        }
+
+        if (err.data && err.data.status === "plugin-dashboard") {
+          err.isHandled = true;
+
+          $scope.appEvent('confirm-modal', {
+            title: 'Plugin Dashboard',
+            text: err.data.message,
+            text2: 'Your changes will be lost when you update the plugin. Use Save As to create custom version.',
+            yesText: 'Overwrite',
+            icon: 'fa-warning',
+            altActionText: 'Save As',
+            onAltAction: function() {
+              dashboardSrv.saveDashboardAs();
+            },
             onConfirm: function() {
               saveDashboard({overwrite: true});
             }
