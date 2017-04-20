c9bf1108e2678ccb409a4a8f67a0de22f73897c1
Carlos Rosquillas <crosquillas@gmail.com>
2017-04-19 22:47:31 -0700

    Use default dashboard save on first save
    - Deduplicate save dashboard error handling

--- public/app/features/dashboard/dashboard_srv.ts
+++ public/app/features/dashboard/dashboard_srv.ts
@@ -23,21 +23,90 @@ export class DashboardSrv {
     return this.dash;
   }
 
-  saveDashboard(options) {
+  afterSave(clone, data) {
+    this.dash.version = data.version;
+
+    var dashboardUrl = '/dashboard/db/' + data.slug;
+    if (dashboardUrl !== this.$location.path()) {
+      this.$location.url(dashboardUrl);
+    }
+
+    this.$rootScope.appEvent('dashboard-saved', this.dash);
+    this.$rootScope.appEvent('alert-success', ['Dashboard saved', 'Saved as ' + clone.title]);
+  }
+
+  handleSaveDashboardError(clone, err) {
+    if (err.data && err.data.status === "version-mismatch") {
+      err.isHandled = true;
+
+      this.$rootScope.appEvent('confirm-modal', {
+        title: 'Conflict',
+        text: 'Someone else has updated this dashboard.',
+        text2: 'Would you still like to save this dashboard?',
+        yesText: "Save & Overwrite",
+        icon: "fa-warning",
+        onConfirm: () => {
+          this.saveDashboard({overwrite: true}, clone);
+        }
+      });
+    }
+
+    if (err.data && err.data.status === "name-exists") {
+      err.isHandled = true;
+
+      this.$rootScope.appEvent('confirm-modal', {
+        title: 'Conflict',
+        text: 'Dashboard with the same name exists.',
+        text2: 'Would you still like to save this dashboard?',
+        yesText: "Save & Overwrite",
+        icon: "fa-warning",
+        onConfirm: () => {
+          this.saveDashboard({overwrite: true}, clone);
+        }
+      });
+    }
+
+    if (err.data && err.data.status === "plugin-dashboard") {
+      err.isHandled = true;
+
+      this.$rootScope.appEvent('confirm-modal', {
+        title: 'Plugin Dashboard',
+        text: err.data.message,
+        text2: 'Your changes will be lost when you update the plugin. Use Save As to create custom version.',
+        yesText: "Overwrite",
+        icon: "fa-warning",
+        altActionText: "Save As",
+        onAltAction: () => {
+          this.saveDashboardAs(clone);
+        },
+        onConfirm: () => {
+          this.saveDashboard({overwrite: true}, clone);
+        }
+      });
+    }
+  }
+
+  saveDashboard(options, clone = this.dash.getSaveModelClone()) {
     if (!this.dash.meta.canSave && options.makeEditable !== true) {
       return Promise.resolve();
     }
 
     if (this.dash.title === 'New dashboard') {
-      return this.saveDashboardAs();
+      return this.saveDashboardAs(clone);
     }
 
-    return this.saveDashboardMessage();
+    if (this.dash.version > 0) {
+      return this.saveDashboardMessage(clone);
+    }
+
+    return this.backendSrv.saveDashboard(clone, options)
+      .then(this.afterSave.bind(this, clone))
+      .catch(this.handleSaveDashboardError.bind(this, clone));
   }
 
-  saveDashboardAs() {
+  saveDashboardAs(clone) {
     var newScope = this.$rootScope.$new();
-    newScope.clone = this.dash.getSaveModelClone();
+    newScope.clone = clone ? clone : this.dash.getSaveModelClone();
     newScope.clone.editable = true;
     newScope.clone.hideControls = false;
 
@@ -48,9 +117,9 @@ export class DashboardSrv {
     });
   }
 
-  saveDashboardMessage() {
+  saveDashboardMessage(clone) {
     var newScope = this.$rootScope.$new();
-    newScope.clone = this.dash.getSaveModelClone();
+    newScope.clone = clone ? clone : this.dash.getSaveModelClone();
 
     this.$rootScope.appEvent('show-modal', {
       src: 'public/app/features/dashboard/partials/saveDashboardMessage.html',
