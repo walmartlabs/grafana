2e4e2b36df5cd0c01abd3fd828cd7b9dacd4ff81
Carlos Rosquillas <crosquillas@gmail.com>
2017-04-27 11:04:31 -0700

    Persist response when switching between deltas

--- public/test/mocks/audit-mocks.js
+++ public/test/mocks/audit-mocks.js
@@ -36,77 +36,8 @@ define([],
         message: '',
       }];
     },
-    compare: function() {
-      return {
-        delta: {
-          Created: [
-            '2017-02-22T17:29:52-08:00',
-            '2017-02-23T22:31:33-08:00',
-          ],
-          CreatedBy: [0, 1],
-          Data: {
-            annotations: {
-              list: {
-                '_0': [
-                  {
-                    datasource: '-- Grafana --',
-                    enable: true,
-                    iconColor: 'rgba(255, 96, 96, 1)',
-                    limit: 100,
-                    name: 'foo',
-                    type: 'alert',
-                  },
-                  0,
-                  0,
-                ],
-                _t: 'a',
-              }
-            },
-            editMode: [false],
-            rows: {
-              0: {
-                panels: {
-                  0: {
-                    links: [
-                      []
-                    ],
-                    targets: {
-                      0: {
-                        alias: [''],
-                        aliasMode: ['default'],
-                        downsampling: ['avg'],
-                        errors: [{ 'metric': 'You must supply a metric name.' }],
-                        groupBy: [{ 'timeInterval': '1s' }],
-                        horAggregator: [
-                          {
-                            factor: '1',
-                            percentile: '0.75',
-                            samplingRate: '1s',
-                            trim: 'both',
-                            unit: 'millisecond',
-                          }
-                        ],
-                        refId: ['A'],
-                      },
-                      _t: 'a'
-                    },
-                    title: ['Panel Title', 'CPU']
-                  },
-                  _t: 'a'
-                }
-              },
-              _t: 'a'
-            },
-            version: [5, 7]
-          },
-          Id: [2, 4],
-          Version: [5, 7]
-        },
-        meta: {
-          new: 7,
-          original: 5
-        }
-      };
+    compare: function(type) {
+      return type === 'basic' ? '<div></div>' : '<pre><code></code></pre>';
     },
     restore: function(version, restoredFrom) {
       return {
